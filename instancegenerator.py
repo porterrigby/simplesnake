fa9d525964712d5b5916c51dc3a80bfecd7993fe
porterrigby <prigby.a@gmail.com>
2025-04-13 00:02:12 -0600

    partially implements master.py

--- instancegenerator.py
+++ instancegenerator.py
@@ -2,11 +2,12 @@ import random
 import string
 from clemcore.clemgame.instances import GameInstanceGenerator
 
-
-GAME_NAME = 'simplesnake'
+MAX_TURNS = 15
 N_INSTANCES = 10
+GAME_NAME = 'simplesnake'
 SEED = -413  # spring, gymnopedie, air :)
-    
+
+
 class SnakeInstanceGenerator(GameInstanceGenerator):
     def __init__(self):
         super().__init__(GAME_NAME)
@@ -19,148 +20,25 @@ class SnakeInstanceGenerator(GameInstanceGenerator):
             experiment = self.add_experiment(matrix)
 
             for game_id in range(N_INSTANCES):
-
-                snake_start_loc = random.choice(range(9))
-                prey_start_loc = random.choice(list(range(9)).remove(snake_start_loc))
-
-                n_turns = 15
-
                 instance = self.add_game_instance(experiment, game_id)
-
-                instance['snake_start_loc'] = snake_start_loc
-                instance['prey_start_loc'] = prey_start_loc
-                instance['n_turns'] = n_turns
-                instace['initial_prompt'] = self.create_prompt(
-                    matrix, 
-                    initial_prompt,
-                    snake_start_loc,
-                    prey_start_loc,
-                    n_turns
-                )
-
-    def create_prompt(matrix, prompt, snake_start_loc, prey_start_loc, n_turns) -> str:
-        text = string.Template(
-            prompt
-        ).substitute(
-            matrix=matrix, 
-            snake_start_loc=snake_start_loc,
-            prey_start_loc=prey_start_loc,
-            n_turns=n_turns
-        )
-
-        return text
-
-if __name__ == '__main__':
-    random.seed(SEED)
-    SnakeInstanceGenerator().generate()
-            t GameInstanceGenerator
-import random
-import string
-from clemcore.clemgame.instances import GameInstanceGenerator
-
-
-GAME_NAME = 'simplesnake'
-N_INSTANCES = 10
-SEED = -413  # spring, gymnopedie, air :)
-    
-class SnakeInstanceGenerator(GameInstanceGenerator):
-    def __init__(self):
-        super().__init__(GAME_NAME)
-
-    def on_generate(self):
-        matrices = self.load_file('resources/matrices.txt').strip('\n').split('\n')
-        initial_prompt = self.load_template('resources/initial_prompts/initial_prompt_a')
-
-        for matrix in matrices:
-            experiment = self.add_experiment(matrix)
-
-            for game_id in range(N_INSTANCES):
-
+                
+                max_turns = MAX_TURNS
+                instance['max_turns'] = max_turns
+                
                 snake_start_loc = random.choice(range(9))
-                prey_start_loc = random.choice(list(range(9)).remove(snake_start_loc))
-
-                n_turns = 15
-
-                instance = self.add_game_instance(experiment, game_id)
-
                 instance['snake_start_loc'] = snake_start_loc
-                instance['prey_start_loc'] = prey_start_loc
-                instance['n_turns'] = n_turns
-                instace['initial_prompt'] = self.create_prompt(
-                    matrix, 
-                    initial_prompt,
-                    snake_start_loc,
-                    prey_start_loc,
-                    n_turns
-                )
-
-    def create_prompt(matrix, prompt, snake_start_loc, prey_start_loc, n_turns) -> str:
-        text = string.Template(
-            prompt
-        ).substitute(
-            matrix=matrix, 
-            snake_start_loc=snake_start_loc,
-            prey_start_loc=prey_start_loc,
-            n_turns=n_turns
-        )
-
-        return text
-
-if __name__ == '__main__':
-    random.seed(SEED)
-    SnakeInstanceGenerator().generate()
-            
-import random
-import string
-from clemcore.clemgame.instances import GameInstanceGenerator
-
-
-GAME_NAME = 'simplesnake'
-N_INSTANCES = 10
-SEED = -413  # spring, gymnopedie, air :)
-    
-class SnakeInstanceGenerator(GameInstanceGenerator):
-    def __init__(self):
-        super().__init__(GAME_NAME)
-
-    def on_generate(self):
-        matrices = self.load_file('resources/matrices.txt').strip('\n').split('\n')
-        initial_prompt = self.load_template('resources/initial_prompts/initial_prompt_a')
-
-        for matrix in matrices:
-            experiment = self.add_experiment(matrix)
-
-            for game_id in range(N_INSTANCES):
-
-                snake_start_loc = random.choice(range(9))
                 prey_start_loc = random.choice(list(range(9)).remove(snake_start_loc))
-
-                n_turns = 15
-
-                instance = self.add_game_instance(experiment, game_id)
-
-                instance['snake_start_loc'] = snake_start_loc
                 instance['prey_start_loc'] = prey_start_loc
-                instance['n_turns'] = n_turns
-                instace['initial_prompt'] = self.create_prompt(
-                    matrix, 
-                    initial_prompt,
-                    snake_start_loc,
-                    prey_start_loc,
-                    n_turns
-                )
 
-    def create_prompt(matrix, prompt, snake_start_loc, prey_start_loc, n_turns) -> str:
-        text = string.Template(
-            prompt
-        ).substitute(
-            matrix=matrix, 
-            snake_start_loc=snake_start_loc,
-            prey_start_loc=prey_start_loc,
-            n_turns=n_turns
-        )
+                instance['describer_tag'] = 'MATRIX:'
+                instance['navigator_tag'] = 'DIRECTION:'
 
-        return text
+                # navigator response pattern (some regex here?)
+                # instance['navigator_response_pattern'] = 
+                
+                instance['describer_initial_prompt'] = self.load_template('resources/initial_prompts/describer_prompt')
+                instance['navigator_initial_prompt'] = self.load_template('resources/initial_prompts/navigator_prompt')
+
 
 if __name__ == '__main__':
     random.seed(SEED)
---
+++ master.py
@@ -0,0 +1,112 @@
+from clemcore.clemgame.benchmark import GameBenchmark
+from clemcore.clemgame.master import GameMaster
+from clemcore.clemgame import Player
+from clemcore.backends.model_registry import Model
+from typing import Dict, Tuple, List
+import logging
+from clemcore.clemgame.master import DialogueGameMaster, Player
+
+logger = logging.getLogger(__name__)
+
+class SnakeGameBenchmark(GameBenchmark):
+    def __init__(self):
+        super().__init__('SNAKE')
+
+    def get_description(self):
+        return "Plays a simple game of snake with one player."
+
+    def is_single_player(self) -> bool:
+        return True
+
+    def create_game_master(self, experiment: Dict, player_models: List[Model]) -> GameMaster:
+        return Snake(self.game_name, self.game_path, experiment, player_models)
+
+
+
+class Navigator(Player):
+    def __init__(self, model: Model):
+        super().__init__(model)
+
+    def _custom_response(self, context):
+        return f"<MOVE UP>"
+
+
+class Describer(Player):
+    def __init__(self, model: Model):
+        super().__init__(model)
+
+    def _custom_response(self, context):
+        return "[][][*]\n[][][]\n[][s][]"
+    
+class Snake(DialogueGameMaster):
+    """ Implements a game of snake in which one player describes the directions in which
+    to move a snake within a grid to find its food.
+    """
+    
+    def __init__(self, game_name: str, game_path: str, experiment: Dict, player_models: List[Model] = None):
+        super().__init__(name, path, experiment, player_models)
+        # READ IN SETTINGS FROM EXPERIMENT/INSTANCE.JSON???
+
+    def _on_setup(self, **game_instance):
+        # describer should be the programmatic player?
+        self.navigator = Navigator(self.player_models[0])
+        self.add_player(self.navigator)
+        self.describer = Describer(self.player_models[1])
+        self.add_player(self.describer)
+
+        self.gameboard = [[None] for _ in range(9)]
+        self.invalid_response = False
+
+        self.game_instance = game_instance
+        self.max_turns = game_instance['max_turns']
+        self.snake_location = game_instance['snake_start_loc']
+        self.prey_location = game_instance['prey_start_loc']
+        self.describer_tag = game_instance['describer_tag']
+        self.navigator_tag = game_instance['navigator_tag']
+        self.describer_initial_prompt = game_instance['describer_initial_prompt']
+        self.navigator_initial_prompt = game_instance['navigator_initial_prompt']
+
+    def _on_before_game(self):
+        self.set_context_for(self.navigator, self.navigator_initial_prompt)
+
+    def _does_game_proceed(self):
+        """Proceed as long as the snake does not occupy the same gridspace as the prey."""
+        if self.invalid_response:
+            self.log_to_self("invalid response", "abort game")
+            return False
+        if self.win_state:
+            self.log_to_self("game win", "end game")
+            return False
+        if self.invalid_state:
+            self.log_to_self("game over", "end game")
+            return False
+        if self.current_turn >= self.max_turns:
+            self.log_to_self("max turns reached", str(self.max_turns))
+            return False
+        return True
+
+    def _validate_player_response(self, player: Player, utterance: str) -> bool:
+
+        self.invalid_response = False
+        self.win_state = False
+        self.invalid_state = False
+        
+        if player == self.navigator:
+            # is navigator response in valid format?    
+            if not (utterance.startswith('<'.append(self.navigator_tag)) and utterance.endswith('>')):
+                self.log_to_self("invalid format", "Invalid response.")
+                self.invalid_format = True
+                return False 
+        # if player == self.describer:
+            # if 
+            #validate response format
+            # pass
+        else:
+            return True
+
+    def _on_valid_player_response(self, player: Player, parsed_response: str):
+        if player == self.navigator:
+            self.set_context_for(self.describer, parsed_response)
+        elif player == self.describer:
+            self.set_context_for(self.navigator, parsed_response)
+            
